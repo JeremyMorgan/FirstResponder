b2566e77882836822797a18c0dcc9a68a1e94585
Jeremy Morgan <jeremymorgan@gmail.com>
2015-04-06 20:12:03 -0700

    Replaced all original files

---
+++ First Responder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First_Responder.Startup))]
+namespace First_Responder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
